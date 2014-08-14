3c7e3cc63d58fc514c1b9610165cecf40830e311
sozgur <sumeyra.ozgur@bil.omu.edu.tr>
2014-08-14 14:15:24 +0300

    hata

--- Rails/s_app/Gemfile
+++ Rails/s_app/Gemfile
@@ -20,7 +20,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',  require: false,        group: :doc
-
+gem 'heroku'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- Rails/s_app/config/routes.rb
+++ Rails/s_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :pages
+  get 'pages/index'
+  match '/index', to: 'pages#index', via: 'get'
   #get '/pages', :to 'pages#deneme'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
