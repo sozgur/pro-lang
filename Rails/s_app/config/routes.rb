419df568439e418266adb296ca026d0a876e2ae8
sozgur <sumeyra.ozgur@bil.omu.edu.tr>
2014-08-14 14:44:28 +0300

    root page

--- Rails/s_app/config/routes.rb
+++ Rails/s_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :pages
+  root :to => 'pages#index'
   get 'pages/index'
   match '/index', to: 'pages#index', via: 'get'
   #get '/pages', :to 'pages#deneme'
